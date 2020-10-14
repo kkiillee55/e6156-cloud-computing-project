7d7c39c756072fb2b7c0d2db550fbdd419a40864
kkiillee55 <53668899+kkiillee55@users.noreply.github.com>
2020-10-14 00:24:17 -0400

    Update __init__.py

--- AWS Elastic Beanstalk/flaskblog/__init__.py
+++ AWS Elastic Beanstalk/flaskblog/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 from flask_mail import Mail
 import os
 app=Flask(__name__)
-app.config['SECRET_KEY']='5dd0ca1eb7b10ea76f4d8665f713471053be329151b6cab8b0b369ac1c605415'
+app.config['SECRET_KEY']=
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 db=SQLAlchemy(app)
 bcrypt=Bcrypt(app)
@@ -18,7 +18,7 @@ login_manager.login_message_category='info'
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT']=587
 app.config['MAIL_USE_TLS']=True
-app.config['MAIL_USERNAME']='zhangliao322@gmail.com'
-app.config['MAIL_PASSWORD']='rtyzkwcwlcmcxoew'
+app.config['MAIL_USERNAME']=
+app.config['MAIL_PASSWORD']=
 mail=Mail(app)
 from flaskblog import routes
